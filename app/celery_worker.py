e4952285dedaba0e38fcc52f18cf0859a6a5f69b
Robert Alexander <gogonegro@gmail.com>
2023-11-03 21:59:02 +0100

    fastapi3 added celery

---
+++ app/celery_worker.py
@@ -0,0 +1,22 @@
+""" Celery task to insert an entry in the Redis cache"""
+# app/celery_worker.py
+from celery import Celery
+import redis
+from app.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
+
+
+# Initialize Celery
+celery_app = Celery(
+    "worker", backend="redis://localhost:6379/0", broker="redis://localhost:6379/0"
+)
+
+
+# Define a Celery task
+@celery_app.task
+def insert_into_cache(hash_key, hash_data):
+    """our Celery task"""
+    redis_client = redis.StrictRedis(
+        host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, decode_responses=True
+    )
+    redis_client.hset(hash_key, mapping=hash_data)
+    redis_client.expire(hash_key, 3000)  # Set TTL for the key.
---
+++ app/config.py
@@ -0,0 +1,7 @@
+""" configuration constants """
+# config.py
+
+# Redis Configuration
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_PASSWORD = ""  # Add your password here if required
